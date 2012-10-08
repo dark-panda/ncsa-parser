bebfcbf41e79d9a8e9cbcfeb132f484b95293c40
J Smith <dark.panda@gmail.com>
2012-10-08 19:35:49 -0300

    Enhance the NCSAParser::ParsedLine#to_hash method.
    This change allows you to pick which attributes you're gathering up.

--- lib/ncsa-parser/parsed_line.rb
+++ lib/ncsa-parser/parsed_line.rb
@@ -8,85 +8,133 @@ module NCSAParser
 
       :request_uri => proc { |match, options|
         begin
-          request = match[:request].scan(/^"[A-Z]+ (.+) HTTP\/\d+\.\d+"$/).flatten[0]
+          request = match.attributes[:request].scan(/^"[A-Z]+ (.+) HTTP\/\d+\.\d+"$/).flatten[0]
           URI.parse("http://#{options[:domain]}#{request}")
         rescue
-        end
+          :bad_uri
+        end if match.attributes[:request]
       },
 
       :request_path => proc { |match, options|
-        match[:request].scan(/^"[A-Z]+ ([^?]+)/).flatten[0] rescue nil
+        match.attributes[:request].scan(/^"[A-Z]+ ([^?]+)/).flatten[0] rescue nil if match.attributes[:request]
       },
 
       :http_method => proc { |match, options|
-        match[:request].scan(/^"([A-Z]+)/).flatten[0] rescue nil
+        match.attributes[:request].scan(/^"([A-Z]+)/).flatten[0] rescue nil if match.attributes[:request]
       },
 
       :http_version => proc { |match, options|
-        match[:request].scan(/(\d+\.\d+)"$/).flatten[0] rescue nil
+        match.attributes[:request].scan(/(\d+\.\d+)"$/).flatten[0] rescue nil if match.attributes[:request]
       },
 
       :query_string => proc { |match, options|
-        if match[:request_uri] && match[:request_uri].query
-          CGI.parse(match[:request_uri].query)
-        else
-          Hash.new
+        if match[:request_uri]
+          if match[:request_uri] && match[:request_uri].query
+            CGI.parse(match[:request_uri].query)
+          else
+            Hash.new
+          end
         end
       },
 
       :referer_uri => proc { |match, options|
-        if match[:referer] != '"-"'
-          referer = match[:referer].sub(/^"(.+)"$/, '\1')
-          NCSAParser::Helper.clean_uri(referer)
+        if match[:referer]
+          if match[:referer] != '"-"'
+            referer = match[:referer].sub(/^"(.+)"$/, '\1')
+            NCSAParser::Helper.clean_uri(referer)
 
-          begin
-            URI.parse(referer)
-          rescue
+            begin
+              URI.parse(referer)
+            rescue
+              :bad_uri
+            end
+          else
+            '-'
           end
-        else
-          '-'
         end
       },
 
       :browscap => proc { |match, options|
-        options[:browscap].match(match[:ua].sub(/^"(.+)"$/, '\1')) if options[:browscap]
+        options[:browscap].query(match[:ua].sub(/^"(.+)"$/, '\1')) if options[:browscap]
       },
 
       :ratio => proc { |match, options|
-        match.attributes[:ratio].to_f / 100 rescue nil
+        match.attributes[:ratio].to_f / 100 rescue nil if match.attributes[:ratio]
+      },
+
+      :host => proc { |match, options|
+        if match.attributes[:host]
+          match.attributes[:host]
+        elsif match.attributes[:host_proxy]
+          match.attributes[:host_proxy].split(',')[0].strip
+        end
       }
     }
 
     %w{ status instream outstream bytes }.each do |field|
       class_eval(<<-EOF, __FILE__, __LINE__ + 1)
         TOKEN_CONVERSIONS[:#{field}] = proc { |match, options|
-          match.attributes[:#{field}].to_i rescue nil
+          match.attributes[:#{field}].to_i rescue nil if match.attributes[:#{field}]
         }
       EOF
     end
 
-    attr_reader :attributes, :parsed_attributes
+    attr_reader :attributes
 
     def initialize(attributes, options = {})
       @attributes, @options = attributes, options
       @parsed_attributes = {}
+
+      if options[:browscap] && !options[:browscap].respond_to?(:query)
+        raise ArgumentError.new("The :browscap object should respond to the #query method.")
+      end
     end
 
+    # Accesses either an attribute or an attribute that has been passed
+    # through a token converter. You can access the raw, unconverted attributes
+    # via the +attributes+ method. If a converter fails for whatever reason,
+    # a value of +:bad_conversion+ is returned.
     def [](key)
       key = key.to_sym unless key.is_a?(Symbol)
 
       if @parsed_attributes.has_key?(key)
         @parsed_attributes[key]
+      elsif @options[:token_conversions] && @options[:token_conversions][key]
+        @parsed_attributes[key] = @options[:token_conversions][key].call(self, @options)
       elsif TOKEN_CONVERSIONS[key]
-        @parsed_attributes[key] = TOKEN_CONVERSIONS[key].call(self, @options)
+        @parsed_attributes[key] = (TOKEN_CONVERSIONS[key].call(self, @options) rescue :bad_conversion)
       else
         @attributes[key]
       end
     end
 
-    def to_hash
-      TOKEN_CONVERSIONS.each { |t, v| self[t] }
-      @attributes.merge(@parsed_attributes)
+    # Gathers up the requested attributes and spits them out into a Hash.
+    # The +values+ argument determines what gets inserted into the Hash:
+    #
+    # * +:all+ - both attributes and parsed attributes. In cases where
+    #   the values share the same names, the parsed attribute wins out.
+    # * +:attributes+ - unparsed attributes only.
+    # * +:parsed+ - parsed attributes only.
+    #
+    # The default value is +:all+. Any +nil+ values are automatically stripped
+    # from the Hash.
+    def to_hash(values = :all)
+      retval = {}
+
+      if values == :all || values == :attributes
+        retval.merge!(@attributes)
+      end
+
+      if values == :all || values == :parsed
+        TOKEN_CONVERSIONS.each { |t, v| self[t] }
+        retval.merge!(@parsed_attributes)
+      end
+
+      retval.reject! { |k, v|
+        v.nil?
+      }
+
+      retval
     end
   end
 end
