b9b0d593e40e7ff172c623757504e56c64837b72
J Smith <dark.panda@gmail.com>
2012-10-08 19:34:49 -0300

    Shore up the NCSAParser::Parser class for a better interface.

--- lib/ncsa-parser/parser.rb
+++ lib/ncsa-parser/parser.rb
@@ -30,18 +30,61 @@ module NCSAParser
       :ratio => '\d+%|-%'
     }
 
-    attr_reader :matcher
+    LOG_FORMAT_COMMON = %w{
+      host ident username datetime request status bytes
+    }
 
-    def initialize(log, pattern = %w{ host ident username datetime request status bytes referer ua }, options = {})
+    LOG_FORMAT_COMBINED = %w{
+      host ident username datetime request status bytes referer ua
+    }
+
+    attr_reader :pattern, :matcher, :re
+
+    # Creates a new Parser object.
+    #
+    # == Options
+    #
+    # * +:domain+ - when parsing query strings, use this domain as the URL's
+    #   domain. The default is +"www.example.com"+.
+    # * +:datetime_format+ - sets the datetime format for when tokens are
+    #   converted in NCSAParser::ParsedLine. The default is +"[%d/%b/%Y:%H:%M:%S %Z]"+.
+    # * +:pattern+ - the default log line format to use. The default is
+    #   +LOG_FORMAT_COMBINED+, which matches the "combined" log format in
+    #   Apache. The value for +:pattern+ can be either a space-delimited
+    #   String of token names or an Array of token names.
+    # * +:browscap+ - a browser capabilities object to use when sniffing out
+    #   user agents. This object should be able to respond to the +query+
+    #   method. Several browscap extensions are available for Ruby, and the
+    #   the author of this extension's version is called Browscapper and is
+    #   available at https://github.com/dark-panda/browscapper .
+    # * +:token_conversions+ - converters to pass along to the line parser.
+    #   See NCSAParser::ParsedLine for details.
+    # * +:tokens+ - tokens to add to the generated Regexp.
+    def initialize(options = {})
       options = {
         :domain => 'www.example.com',
         :datetime_format => '[%d/%b/%Y:%H:%M:%S %Z]',
-        :browscap => nil
+        :pattern => LOG_FORMAT_COMBINED
       }.merge(options)
 
-      @log, @pattern, @options = log, pattern, options
+      @options = options
+      @pattern = if options[:pattern].is_a?(Array)
+        options[:pattern]
+      else
+        options[:pattern].to_s.split(/\s+/)
+      end
+
       @re = '^' + @pattern.collect { |tk|
-        "(#{TOKENS[tk.to_sym]})"
+        tk = tk.to_sym
+        token = if options[:tokens] && options[:tokens][tk]
+          options[:tokens][tk]
+        elsif TOKENS[tk]
+          TOKENS[tk]
+        else
+          raise ArgumentError.new("Token :#{tk} not found!")
+        end
+
+        "(#{token})"
       }.join(' ') + '$'
       @matcher = Regexp.new(@re)
     end
