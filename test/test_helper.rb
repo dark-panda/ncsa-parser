d12a555d4fce18eca89f2203b7095d92403b2cff
J Smith <dark.panda@gmail.com>
2012-11-23 12:04:55 -0500

    minitest-reporters doesn't actually work with ruby 1.8.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,11 @@
 
 require 'rubygems'
 require 'minitest/autorun'
-require 'minitest/reporters'
+
+if RUBY_VERSION >= '1.9'
+  require 'minitest/reporters'
+end
+
 require File.join(File.dirname(__FILE__), %w{ .. lib ncsa-parser })
 
 puts "NCSAParser version #{NCSAParser::VERSION}"
@@ -16,5 +20,7 @@ module TestHelper
   LOG_DEFLATE = %{123.123.123.123 - - [08/Oct/2012:14:36:07 -0400] "GET /path/to/something?foo=bar&hello=world HTTP/1.1" 200 923 "http://www.example.com/referer" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4" 905 1976 45%}
 end
 
-MiniTest::Reporters.use!(MiniTest::Reporters::SpecReporter.new)
+if RUBY_VERSION >= '1.9'
+  MiniTest::Reporters.use!(MiniTest::Reporters::SpecReporter.new)
+end
 
